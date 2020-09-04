037caf9d8e59e82132d9aca2ae9dd08bb0bd30dd
mylesmcs <info@mylesmcsweeney.com>
2020-09-04 22:30:48 +0100

    API - update endpoint fix

--- api/app/views.py
+++ api/app/views.py
@@ -6,33 +6,9 @@ import os
 # curl -d '{"ip":"10.0.0.1","hostname":"test"}' -H "Content-Type: application/json" -X POST -i http://localhost:5000/dyn/api/update
 #178.79.150.36
 
-SERVER = '127.0.0.1'
-ZONE = 'dyn.example.com'
+SERVER = 'bind'
 
-cmd = f'server {SERVER}\n' \
-    f'zone {ZONE}\n'
-    
-print(cmd)
-
-command = f"nsupdate << EOF\n{cmd}\nEOF\n"
-
-subprocess.call(command, shell=True)
-
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 @app.route("/")
 def index():
@@ -48,8 +24,20 @@ def index():
 
 @app.route('/dyn/api/update', methods=['POST'])
 def get_test():
+    #get params from POST request
     data = request.get_json()
-    ipAddr = data['ip']
-    hostName = data['hostname']
-    print(f"{hostName} --- {ipAddr}")
+    ip = data['ip']
+    zone = data['zone']
+    # action to be performed
+    action = f'server {SERVER}\n' \
+    f'update add {zone} 3600 A {ip}\n' \
+    'send\n'
+    # run above action
+    run_cmd(action)
     return ('OK')
+
+def run_cmd(action):
+    command = f"nsupdate -k ./ddns-key << EOF\n{action}\nEOF\n"
+    process = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    return process.stdout
+
