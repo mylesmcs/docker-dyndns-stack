fcc3945ec5c0ff9090d3e439b0da97d6629db56c
mylesmcs <info@mylesmcsweeney.com>
2020-09-05 13:20:31 +0100

    add db

---
+++ api/application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# App Init
+app = Flask(__name__)
+
+# Db Init
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from application import views
+
--- api/application/views.py
+++ api/application/views.py
@@ -1,14 +1,15 @@
-from app import app
+from application import app, db
 from flask import jsonify, request
+from app.model import User
 import subprocess
 import os
 
-# curl -d '{"ip":"10.0.0.1","hostname":"test"}' -H "Content-Type: application/json" -X POST -i http://localhost:5000/dyn/api/update
+# curl -d '{"token":"123456","ip":"10.0.0.1","zone":"test.dyn.example.com"}' -H "Content-Type: application/json" -X POST -i http://localhost:5000/dyn/api/update
 #178.79.150.36
 
 SERVER = 'bind'
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 @app.route("/")
 def index():
@@ -26,18 +27,25 @@ def index():
 def get_test():
     #get params from POST request
     data = request.get_json()
-    ip = data['ip']
-    zone = data['zone']
+    try:
+        token = data['token']
+        ip = data['ip']
+        zone = data['zone']
+    except:
+        return 'Not all params have been supplied'
     # action to be performed
     action = f'server {SERVER}\n' \
-    f'update add {zone} 3600 A {ip}\n' \
+    f'update add {zone} 60 A {ip}\n' \
     'send\n'
     # run above action
     run_cmd(action)
+    user_update(token, zone, ip)
     return ('OK')
 
 def run_cmd(action):
-    command = f"nsupdate -k ./ddns-key << EOF\n{action}\nEOF\n"
+    command = f"nsupdate -k {os.path.dirname(os.path.abspath(__file__))}/ddns-key << EOF\n{action}\nEOF\n"
     process = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
     return process.stdout
 
+def user_update(token, zone, ip):
+    print(f'{token} -- {zone} -- {ip}')
